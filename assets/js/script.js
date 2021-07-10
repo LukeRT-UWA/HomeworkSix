d1d5801db71072180d422e9fd5b4849e55267b0f
Luke Thompson <16175583curtin@gmail.com>
2021-07-10 18:38:38 +0800

    weather information displays in main section except for icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const apiKey = "bbc2b5e33bc93b1c9b2424b433881299"
-
+var currentTime = moment().format("DD" + "/" + "MM" + "/" + "YYYY");
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 function getCityForecast (cityName) {
@@ -8,7 +8,7 @@ function getCityForecast (cityName) {
     .then( (data) => data.json())
     .then(function(data){
         console.log(data);
-        console.log(data.main.humidity)
+        
         const lon = data.coord.lon;
         const lat = data.coord.lat;
         return getOneCall(lat, lon)
@@ -47,7 +47,29 @@ function search(event){
             console.log(original, onecallData);
 
             // populate the today's weather section
-
+            // City name
+            document.getElementById('main-city').textContent = (userInput + " ")
+             // Temperature
+            let todayTemp = original.main.temp;
+            let todayTempC = (todayTemp - 273);
+            let todayTempFinal = todayTempC.toFixed(2)
+            // Wind
+            document.getElementById('main-temperature').textContent = ("Temp: " + todayTempFinal + "°C")
+            let todayWind = original.wind.speed
+            let todayWindKPH = (todayWind * 1.609344)
+            let todayWindFinal = todayWindKPH.toFixed(2)
+            document.getElementById('main-wind').textContent = ("Wind: " + todayWindFinal + " Km/H")
+            // Humidity
+            let todayHumidity = original.main.humidity
+            document.getElementById('main-humidity').textContent = ("Humidity: " + todayHumidity + " %")
+            console.log(todayHumidity)
+            // UV Index
+            let todayUVI = onecallData.current.uvi
+            document.getElementById('main-UVI').classList.remove("hide")
+            document.getElementById('main-UVI').textContent = (todayUVI)
+            // Date
+            document.getElementById('main-date').textContent = currentTime
+            // Icon
 
             // populate 5 days section
 
@@ -56,3 +78,4 @@ function search(event){
 }
 
 document.getElementById('search-form').addEventListener("submit", search);
+
